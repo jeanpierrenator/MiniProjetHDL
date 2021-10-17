dc936bd5269012f232fe3d96c52f50ff4e10b3dc
Guillaume Ropion <58080970+jeanpierrenator@users.noreply.github.com>
2021-10-17 21:34:47 +0200

    Update Loto.v

--- ProjetFinalHDL/Loto.v
+++ ProjetFinalHDL/Loto.v
@@ -1,17 +1,41 @@
-module Loto (Clk, BPBack, BPReview, BPReset, BPTirage, A1, A2, A3, A4); 
+odule Loto (Clk, BPBack, BPReview, BPReset, BPTirage, A1, A2, A3, A4);
+input Clk, BPBack, BPReview, BPReset, BPTirage;
+output [6:0] A1;
+output [6:0] A2;
+output [6:0] A3;
+output [6:0] A4;
 
-input Clk, BPBack, BPReview, BPReset, BPTirage; 
+wire [6:0] A, [6:0] B, [6:0] C, [6:0] D, [6:0] E, [6:0] F, [6:0] G, [6:0] H, [6:0] I, [6:0] J;
 
-output [6:0] A1; 
+CmpModuloReglable#()
+	CntModReg(Clk, nSet, nReset, modulo, S);
 
-output [6:0] A2; 
+GestionAd#()
+	GestionAdresse(RetourArriere, Mode, AdMax, AdOut1, AdOut2);
 
-output [6:0] A3; 
+ModuleTirage#()
+	ModTir(TriggerTirage, CntModuloReg, NbrTire) ;
 
-output [6:0] A4; 
+Memory#()
+	MemLibre(AddIn, DataBusIn, DataBusOut, Latch, R/W) ;
+	MemSave(AddIn, DataBusIn, DataBusOut, Latch, R/W) ;
 
- 
+GestionMemLibre#()
+GestionMemoireLibre(AddIn, DataBusIn, DataBusOut, Latch, R/W, DataOut, AdModulo, AdMax, Clk, Reset);
 
-wire [6:0] A, [6:0] B, [6:0] C, [6:0] D, [6:0] E, [6:0] F, [6:0] G, [6:0] H, [6:0] I, [6:0] J; 
+CntTours#()
+	CntToursJoué(nReset, nSet, Clk, S);
+
+GestionMemoireSauvegardé#()
+	GestionMemSave(AddIn, DataOut, DataIn, Clk, AddOut, DataBusIn, DataBusOut, Latch, R/W);
+
+ConvertisseurBCD#()
+	ConvAff12(BinaireIn, BCDOut);
+	ConvAff34(BinaireIn, BCDOut);
+
+Aff7SegHexa#()
+	Aff12(Byte, Msq, Lsq);
+	Aff34(Byte, Msq, Lsq);
+
 
 endmodule
